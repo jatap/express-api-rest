71379faf70a8f61e86db29d9d646744c10694108
Julio Antúnez Tarín <julio@giffgaff.co.uk>
2020-04-16 18:23:15 +0100

    feat(core): add compression middleware

--- generator/server/app.js
+++ generator/server/app.js
@@ -3,6 +3,7 @@ import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import Boom from '@hapi/boom';
 import cors from 'cors';
+import compression from 'compression';
 import api from './api';
 
 const app = express();
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors());
+app.use(compression());
 
 app.use('/api', api);
 

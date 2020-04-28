333f93da3890ae673162f138300e1cb72912e25d
Julio Antúnez Tarín <julio@giffgaff.co.uk>
2020-04-29 00:55:01 +0100

    refactor(errors): delete @hapi/boom

--- generator/server/app.js
+++ generator/server/app.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
-import Boom from '@hapi/boom';
 import cors from 'cors';
 import compression from 'compression';
 import HttpStatus from 'http-status-codes';
@@ -34,32 +33,20 @@ app.use('/api', api);
 
 app.use((req, res, next) => {
   /* eslint-disable prefer-const */
-  const err = Boom.notFound(`${req.method} ${req.url} Not Found`);
+  const err = new Error(`${req.method} ${req.url} Not Found`);
+  err.status = HttpStatus.NOT_FOUND;
   next(err);
 });
 
 /* eslint-disable no-unused-vars */
 app.use((err, req, res, next) => {
-  if (Boom.isBoom(err)) {
-    /* istanbul ignore next */
-    if (config.util.getEnv('NODE_ENV') !== 'test') {
-      console.error(err.output.payload);
-    }
-
-    res.status(
-      err.output.payload.statusCode || HttpStatus.INTERNAL_SERVER_ERROR,
-    );
-
-    res.json(err.output.payload);
-  } else {
-    /* istanbul ignore next */
-    if (config.util.getEnv('NODE_ENV') !== 'test') {
-      console.error(`*** Error: "${err.message}" ***`);
-    }
-
-    res.status(err.status || HttpStatus.INTERNAL_SERVER_ERROR);
-    res.json({ error: err.message });
+  /* istanbul ignore next */
+  if (config.util.getEnv('NODE_ENV') !== 'test') {
+    console.error(`*** Error: "${err.message}" ***`);
   }
+
+  res.status(err.status || HttpStatus.INTERNAL_SERVER_ERROR);
+  res.json({ error: err.message });
 });
 
 export default app;
--- generator/server/tests/app.spec.js
+++ generator/server/tests/app.spec.js
@@ -1,88 +1,16 @@
+import HttpStatus from 'http-status-codes';
 import request from 'supertest';
-import Boom from '@hapi/boom';
 import app from '../app';
 
 describe('app', () => {
   test('not found', async () => {
     const res = await request(app).get('/invalid-route');
 
-    expect(res.status).toBe(404);
-    expect(res.body.error).toBe('Not Found');
+    expect(res.status).toBe(HttpStatus.NOT_FOUND);
+    expect(res.body.error).toEndWith('Not Found');
   });
 
-  test('error using Boom with status code assigned', async () => {
-    /* eslint-disable no-underscore-dangle */
-    const router = app._router;
-
-    /* eslint-disable no-unused-vars */
-    router.get('/sample1', (req, res, next) => {
-      const err = Boom.internal();
-      err.output.payload.statusCode = 503;
-      next(err);
-    });
-
-    const newStack = [];
-    const anonymous = [];
-    const boundDispatch = [];
-
-    /* eslint-disable no-underscore-dangle, array-callback-return */
-    router.stack.map((layer) => {
-      if (layer.name !== '<anonymous>' && layer.name !== 'bound dispatch') {
-        newStack.push(layer);
-      } else if (layer.name === '<anonymous>') {
-        anonymous.push(layer);
-      } else if (layer.name === 'bound dispatch') {
-        boundDispatch.push(layer);
-      }
-    });
-
-    boundDispatch.map((item) => newStack.push(item));
-    anonymous.map((item) => newStack.push(item));
-
-    router.stack = newStack;
-
-    const response = await request(app).get('/sample1');
-
-    expect(response.status).toBe(503);
-  });
-
-  test('error using Boom without status code assigned', async () => {
-    /* eslint-disable no-underscore-dangle */
-    const router = app._router;
-
-    /* eslint-disable no-unused-vars */
-    router.get('/sample2', (req, res, next) => {
-      const err = Boom.internal();
-      err.output.payload.statusCode = undefined;
-      next(err);
-    });
-
-    const newStack = [];
-    const anonymous = [];
-    const boundDispatch = [];
-
-    /* eslint-disable no-underscore-dangle, array-callback-return */
-    router.stack.map((layer) => {
-      if (layer.name !== '<anonymous>' && layer.name !== 'bound dispatch') {
-        newStack.push(layer);
-      } else if (layer.name === '<anonymous>') {
-        anonymous.push(layer);
-      } else if (layer.name === 'bound dispatch') {
-        boundDispatch.push(layer);
-      }
-    });
-
-    boundDispatch.map((item) => newStack.push(item));
-    anonymous.map((item) => newStack.push(item));
-
-    router.stack = newStack;
-
-    const response = await request(app).get('/sample2');
-
-    expect(response.status).toBe(500);
-  });
-
-  test('error without using Boom implementation with no status code', async () => {
+  test('error with no status code', async () => {
     /* eslint-disable no-underscore-dangle */
     const router = app._router;
 
@@ -119,7 +47,7 @@ describe('app', () => {
     expect(response.status).toBe(500);
   });
 
-  test('error without using Boom implementation with status code', async () => {
+  test('error with status code', async () => {
     /* eslint-disable no-underscore-dangle */
     const router = app._router;
 
